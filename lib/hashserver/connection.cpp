664d7f40772df7a13675d2657c8ae4ff622b854f
Tamás Szelei <szelei.t@gmail.com>
2023-06-11 00:18:04 +0200

    connection: simplify and fix processing
    https://github.com/tamas-nx-test/hashserver/issues/5

--- lib/hashserver/connection.cpp
+++ lib/hashserver/connection.cpp
@@ -1,5 +1,4 @@
 #include <cstddef>
-#include <fmt/core.h>
 #include <iterator>
 #include <memory>
 
@@ -7,17 +6,29 @@
 #include <boost/asio/read.hpp>
 #include <boost/asio/streambuf.hpp>
 #include <boost/asio/write.hpp>
-#include <boost/tokenizer.hpp>
+#include <boost/range/iterator_range.hpp>
 #include <boost/utility/string_view.hpp>
 #include <fmt/printf.h>
 
 #include "hashserver/connection.hpp"
-
-using std::begin;
-using std::end;
-using std::next;
+#include "hashserver/hasher.hpp"
 
 namespace hss {
+
+namespace {
+
+void send_hash(boost::asio::ip::tcp::socket &socket, Hasher &hasher) {
+  auto hash = std::make_shared<std::string>(hasher.finalize());
+  boost::asio::async_write(
+      socket, boost::asio::buffer(*hash),
+      [hash](boost::system::error_code ec, std::size_t /*length*/) {
+        if (!ec) {
+          fmt::print("Sent hash: {}\n", *hash);
+        }
+      });
+}
+
+} // namespace
 
 Connection::Connection(boost::asio::ip::tcp::socket socket)
     : socket_(std::move(socket)) {
@@ -32,52 +43,19 @@ void Connection::do_read() {
       boost::asio::buffer(input_buffer_.data(), input_buffer_.size()),
       [this, self](boost::system::error_code ec, std::size_t length) {
         if (!ec) {
-          // special case: a single character that is a newline
-          if (length == 1 && input_buffer_[0] == '\n') {
-            auto hash = std::make_shared<std::string>(hasher_.finalize());
-            boost::asio::async_write(
-                socket_, boost::asio::buffer(*hash),
-                [this, self, hash](boost::system::error_code ec,
-                                   std::size_t /*length*/) {
-                  if (!ec) {
-                    fmt::print("Sent hash: {}\n", *hash);
-                  }
-                });
-            do_read();
-            return;
-          }
+          fmt::print("Read {} bytes\n", length);
 
-          // we have read some amount of characters, some of which may be
-          // newlines
-          std::string text(input_buffer_.data(), length);
-          boost::char_separator<char> sep("\n");
-          boost::tokenizer<boost::char_separator<char>> tokens(text, sep);
-          bool terminated = text.back() == '\n';
-          auto num_tokens = std::distance(begin(tokens), end(tokens));
-
-          for (auto it = begin(tokens); it != end(tokens); ++it) {
-            fmt::print("Received token: {}\n", *it);
-            hasher_.update(*it);
-
-            bool is_last = next(it) == end(tokens);
-
-            if (!is_last || terminated) {
-              // the last token is potentially an incomplete line (no newline at
-              // the end) so only finalize and send back the hash if it's
-              // complete. We only want to send back the hash if we have a
-              // complete line. If it's not the last token, we know it's a
-              // complete line because we split on newlines.
-              auto hash = std::make_shared<std::string>(
-                  fmt::format("{}\n", hasher_.finalize()));
-              boost::asio::async_write(
-                  socket_, boost::asio::buffer(*hash),
-                  [this, self, hash](boost::system::error_code ec,
-                                     std::size_t /*length*/) {
-                    if (!ec) {
-                      fmt::print("Sent hash: {}", *hash);
-                    }
-                  });
+          // process the input buffer one byte at a time
+          for (auto &it : boost::make_iterator_range(
+                   input_buffer_.data(), input_buffer_.data() + length)) {
+            // Whenever we encounter a newline, we send back the hash of the
+            // currently accumulated data (excluding the newline). This also
+            // resets the hasher.
+            if (it == '\n') {
+              send_hash(socket_, hasher_);
+              continue;
             }
+            hasher_.update(it);
           }
         }
         do_read();
