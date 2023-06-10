d9105b9d9ca1643f3fbb00be5b8231c308a903ee
Tamás Szelei <szelei.t@gmail.com>
2023-06-10 22:24:11 +0200

    Extend integration tests with any byte sequence

--- integration_tests.py
+++ integration_tests.py
@@ -8,6 +8,7 @@ test_cases = {
     "Hello, World": "907D14FB3AF2B0D4F18C2D46ABE8AEDCE17367BD",
     "The quick brown fox jumps over the lazy dog": "2FD4E1C67A2D28FCED849EE1BB76E7391B93EB12",
     "a " * 50000: "49A80174FA692B366216DBEFE94851ACBA042410",
+    "русский": "10C8E649725EE2829541600CF8528D91306C5AAA",
 }
 
 
@@ -16,7 +17,7 @@ def send_data(data: str) -> str:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         try:
             sock.connect((HOST, PORT))
-            sock.sendall(bytes(data + "\n", "utf-8"))
+            sock.sendall(f"{data}\n".encode("utf-8"))
             received = str(sock.recv(1024), "utf-8")
         except socket.error as e:
             print("Socket error: ", e)
@@ -25,6 +26,25 @@ def send_data(data: str) -> str:
         finally:
             sock.close()
     return received.strip()
+
+
+def send_all_bytes() -> bool:
+    HOST, PORT = 'localhost', 8001
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        try:
+            sock.connect((HOST, PORT))
+            for b in range(256):
+                data = bytearray()
+                data.append(b)
+                data.extend(b"\n")
+                sock.sendall(data)
+                sock.recv(1024)
+        except socket.error as e:
+            print("Socket error: ", e)
+        except Exception as e:
+            print("Other exception: ", e)
+        finally:
+            sock.close()
 
 
 def test() -> bool:
@@ -45,7 +65,12 @@ def test() -> bool:
 if __name__ == "__main__":
     import sys
 
+    print("\n*** Sending all bytes from 0x00..0xff ***")
+    send_all_bytes()
+
+    print("\n*** Sending known strings ***")
     passed = test()
     print("\n*** All tests passed! ***" if passed else "*** Some tests failed! ***")
+
 
     sys.exit(0 if passed else 1)
